da232626d629896055e6e423adb8ca571d16afbf
PRIYARANJAN JENA <jena.priya13@gmail.com>
2023-04-13 05:17:07 +0000

    13 aprl

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const userId = 15563
+let userEmail = "joha@gmail.com"
+var userPassword = "1432"
+let userCity
+
+userEmail = "jj@gmail.com"
+userPassword = "9876"
+
+console.log(userId);
+console.log(userEmail);
+console.log(userPassword);
+
+
+
+console.table([userId, userEmail, userPassword, userCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict" // treat all js code as a newear version
+
+alert(2 +2)  // it will show a error because we using node js not in browser
+
+
+// Number
+// BigInt
+// String
+// Boolean >> true/false
+// null >> standalone value  (null is an object)
+// undefined
+// symbol >> unique
