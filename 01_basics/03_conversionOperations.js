b56507e3ce2fb6b2a6482489841e9ddc5736f146
PRIYARANJAN JENA <jena.priya13@gmail.com>
2023-04-14 07:30:27 +0000

    added

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,20 @@
+let name = "priyaranjan"
+
+// console.log(typeof name);  // >> String
+
+// let nameInNumber = Number(name)
+// console.log(typeof nameInNumber);  // >> Number
+// console.log(nameInNumber);   // >> NaN
+
+
+/* -------------------------Operations------------------------ */
+
+let x = 4
+let y = x++
+
+console.log(x, y);  // (output: 5 4)the increment operator increments and returns the value before incrementing.
+
+let a = 5
+let b = ++a
+
+console.log(a, b);  // (output: 6 6)the increment operator increments and returns the value after incrementing.
