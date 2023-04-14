c8764a1bed76fdf0f2eb32eb9a7a10caad4012db
PRIYARANJAN JENA <jena.priya13@gmail.com>
2023-04-14 07:46:35 +0000

    addded

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,12 @@
+console.log(4 > 2);
+
+// this type of convesions must not be practiced 
+
+console.log(null > 1);
+console.log(null >= 1);
+console.log(null >= 0);
+console.log(null == 1);
+
+console.log(undefined > 1);
+console.log(undefined >= 1);
+console.log(undefined == 1);
