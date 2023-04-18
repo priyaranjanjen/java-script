9ac5a64ade79eded2b6009e02d5e0fd65ceb4619
PRIYARANJAN JENA <jena.priya13@gmail.com>
2023-04-18 06:01:32 +0000

    comp

---
+++ 01_basics/07_numsANDmaths.js
@@ -0,0 +1,28 @@
+const health = 100
+// console.log(health);
+// console.log(typeof health);
+
+const balance = new Number(200);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));   //>> use in ecommerce website..
+
+const number = 746.97339
+// console.log(number.toPrecision(3));
+
+
+
+//----------------- MATHS -----------------
+
+
+// console.log(Math.PI);
+// console.log(Math.abs(-6));
+// console.log(Math.round(123.6544));
+// console.log(Math.ceil(123.6544));
+// console.log(Math.floor(123.6544));
+
+const max =20
+const min =10
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
