bc4258c6591401744667ac36a179e2fc9712ce7c
PRIYARANJAN JENA <jena.priya13@gmail.com>
2023-04-20 10:11:12 +0000

    20aprl

---
+++ 03_basics/01_conditionals_SIgnIn.js
@@ -0,0 +1,17 @@
+let email = false
+let google = false
+let facebook = true
+
+// if (email) {
+//     console.log("01 LoggedIn");
+// }
+// if (google) {
+//     console.log("02 LOggedIn");
+// }
+// if (facebook) {
+//     console.log("03 LOggedIn");
+// }
+
+if (email || google || facebook) {
+    console.log("LogIn Sucesss");
+}
---
+++ 03_basics/02_conditionals_Authentication.js
@@ -0,0 +1,10 @@
+
+let authentication = true
+
+// if (authentication) {
+//     console.log("succesfull");
+// } else {
+//     console.log("not succesfull");
+// }
+
+authentication ? console.log("succesfull") : console.log("not succesfull");
